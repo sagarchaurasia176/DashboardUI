2d934ecf58f21d827fd5b76b37f8336849af7313
manish-lal12 <manishlal2274@gmail.com>
2024-11-30 00:55:53 +0530

    Added Component Highlight Code

---
+++ app/src/pages/CodePreview.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrettyCode from "rehype-pretty-code";
+
+export function CodePreview({ code }: { code: string }) {
+  const [highlightedCode, setHighlightedCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Function to highlight code asynchronously
+    const highlight = async () => {
+      const highlighted = await highlightCode(code);
+      setHighlightedCode(highlighted);
+    };
+
+    highlight();
+  }, [code]);
+
+  if (highlightedCode === null) {
+    return <div>Loading...</div>; // You can show a loading state here
+  }
+
+  return (
+    <section
+      dangerouslySetInnerHTML={{
+        __html: highlightedCode,
+      }}
+    />
+  );
+}
+
+async function highlightCode(code: string) {
+  const wrappedCode = `\`\`\`jsx\n${code}\n\`\`\``;
+
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypePrettyCode, {
+      keepBackground: true,
+    })
+    .use(rehypeStringify)
+    .process(wrappedCode);
+
+  return String(file);
+}
