b91d90ce7ff62d63e65d464ce4b59afd73cacaaa
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-03-04 15:16:21 +0530

    fetal:add ratelimit middleware for express

--- server/index.ts
+++ server/index.ts
@@ -2,24 +2,49 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./connection/db";
+import { rateLimit } from 'express-rate-limit'
+
 // import route handlers
 import { paymentRoutes, productRoutes, userRoutes } from "./routes/index";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { handleStripeWebhook } from "./controllers/payment";
+import helmet from "helmet";
 
+// app - express()
 const app = express();
+app.use(helmet());
+// Configure the Content-Security-Policy header.
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "example.com"],
+    },
+  })
+);
+
+
+// rate limiter
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
 
 const port = process.env.PORT || 4000;
-
 app.use(
   cors({
     credentials: true,
     origin: "http://localhost:5173",
   })
 );
-
-// Check it for stripe
+// Check it for stripe 
 app.use(
   "/api/v1/payment/webhook",
   express.raw({ type: "application/json" }),
@@ -27,9 +52,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-
 app.use(express.static(path.join(__dirname, "../app/dist")));
-
 //route handlers
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);
@@ -42,5 +65,4 @@ const start = async () => {
     console.log(`Server is listening on port: ${port}...`);
   });
 };
-
 start();
