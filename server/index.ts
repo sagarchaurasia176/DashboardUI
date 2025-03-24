f95b4604e74e89d555f19f66c88d7b8913759da0
manish-lal12 <manishlal2274@gmail.com>
2025-03-24 09:53:55 +0530

    FEAT: Added dockercompose

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import https from "https";
-import fs from "fs";
 import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./connection/db";
@@ -18,24 +17,19 @@ app.use(helmet());
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      "script-src": ["'self'", "example.com"],
+      "script-src": ["'self'"],
     },
   })
 );
 
-// rate limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below.
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
 });
 
-// Apply the rate limiting middleware to all requests.
 app.use(limiter);
-
-const port = process.env.PORT;
 
 app.use(
   cors({
@@ -43,24 +37,25 @@ app.use(
     origin: checkAllowedOrigin,
   })
 );
-// Check it for stripe
+
+// stripe route
 app.use(
   "/api/v1/payment/webhook",
   express.raw({ type: "application/json" }),
   handleStripeWebhook
 );
-// TODO remove it later
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
-//route handlers
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 
-//error handler middleware
+// error handler
 app.use(errorHandler);
+
+const port = process.env.PORT || 4000;
 
 const start = async () => {
   try {
